2a24de1efe915199cc27234924cf2d6c2bda9ebc
Tsung-Yu-Hsieh <31909068+Tsung-Yu-Hsieh@users.noreply.github.com>
2018-01-06 01:06:53 +0800

    Delete Shell.cc

--- Shell.cc
+++
@@ -1,9 +0,0 @@
-$ cat Shell.cc
-#include <iostream>
-int main(int argc, char **argv)
-{
-  std::cout << "Hello, World!!\n"
-            << argc << "\n" << argv[1] << " " << argv[2]
-            << std::endl;
-  return 0;
-}
