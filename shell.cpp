363b994668e6e1d2576810594128dc0c6d3c35fb
Tsung-Yu-Hsieh <31909068+Tsung-Yu-Hsieh@users.noreply.github.com>
2018-01-14 23:52:01 -0800

    Add files via upload

--- shell.cpp
+++ shell.cpp
@@ -7,31 +7,50 @@ using namespace std;
 
 int main( int argc , char **argv )
 {
-  string input;
-  string Input;
-  cout << "Welcome to Atom-Prolog!!" << '\n';
-  cout << "?- ";
-    while(getline (cin,input)){
-      if(input.length()==0){
-        cout << "?- ";
-        }
-      else if(input.find(".", 0)!=string::npos){
-              Input = Input+input;
-              Scanner s(Input);
-              Parser p(s);
-              p.buildExpression();
-              string result = p.getExpressionResult();
-              std::cout << result << '\n';
-                  Input="";
-              result="";
-              cout << "?- ";
+ string input;
+ string Input;
+ string result;
+ cout << "Welcome to Atom-Prolog!!" << '\n';
+ cout << "?- ";
+   while(getline (cin,input)){
+       if(input=="halt."){
+         break;
+       }
+       else if(input.find(";")!=-1 && input.find(".") == -1){
+         Input = Input + input + " ";
+         cout << "|   ";
+       }
+       else if(input.find(".", 0)!=string::npos){
+             Input=Input+input;
+             Scanner s(Input);
+             Parser p(s);
+            try {
+              p.buildExpression(); // according to your implementation.
+              result = p.getExpressionResult();
+            } catch (std::string &msg) {
+              result = msg;
             }
-      else if(input=="halt."){
-          break;
-        }
-        else{
-          Input = Input+input;
-          cout << "|  " ;
-        }
+             std::cout << result << '\n';
+             Input="";
+             result="";
+             cout << "?- ";
     }
+       else if(input.length()==0 && Input.length()==0){
+         cout << "?- ";
+       }
+       else{
+         try {
+           Input=Input+" "+input;
+           Scanner s(Input);
+           Parser p(s);
+           p.buildExpression(); // according to your implementation.
+           result = p.getExpressionResult();
+           cout << "|   " ;
+         } catch (std::string &msg) {
+           result = msg;
+           cout << result << '\n';
+           std::cout << "|   ";
+         }
+       }
+   }
 }
